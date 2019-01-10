c85bd23a2d55bed5308ac227e41a239e5b6128ea
wb-cc346272 <wb-cc346272@antfin.com>
2019-01-10 16:24:49 +0800

    clear

--- src/models/home.js
+++ src/models/home.js
@@ -22,19 +22,5 @@ reducers: {
 */
 
 export default {
-
   namespace: 'home',
-
-  state: {
-    theme: "dark",
-    collapsed: false,
-  },
-
-  reducers: {
-    switchTheme(state, {payload}) {
-      state.theme = payload.theme;
-      return state
-    },
-  },
-
 }
