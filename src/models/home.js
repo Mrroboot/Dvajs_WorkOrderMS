0b892d12ab3fe60f9b0eab421bda5cec357be29d
wb-cc346272 <wb-cc346272@antfin.com>
2019-01-10 16:23:55 +0800

    clear

--- src/models/home.js
+++ src/models/home.js
@@ -1,3 +1,26 @@
+/* model文件格式参考
+namespace: 'example',
+
+  state: {},
+
+subscriptions: {
+  setup({ dispatch, history }) {  // eslint-disable-line
+  },
+},
+
+effects: {
+*fetch({ payload }, { call, put }) {  // eslint-disable-line
+    yield put({ type: 'save' });
+  },
+},
+
+reducers: {
+  save(state, action) {
+    return { ...state, ...action.payload };
+  },
+},
+*/
+
 export default {
 
   namespace: 'home',
